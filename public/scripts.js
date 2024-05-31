f51fc9f33d6f9bb03c684be9096410660a1e067f
My-Epic-Github <bananazzzzz020@gmail.com>
2024-05-31 17:51:32 -0600

    getting the basic template down for the website before i add any of the long monologues or information

---
+++ public/scripts.js
@@ -0,0 +1,39 @@
+//scripts go here
+console.log("loaded script");
+const body = document.getElementById("body");
+const content = document.getElementById("content");
+const sidebar = document.getElementById("sidebar");
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+
+
+//window load function
+window.onload = () => {
+  w3_close();
+
+  //stops loading transitions
+  setTimeout(() => {
+    [sidebar, body].forEach(e => {
+      e.classList.remove("preload");
+    });
+  }, 1);
+}
+
+
+//sidebar function definitions
+function w3_open() {
+  console.log("open called");
+  body.style.marginLeft = "25%";
+  content.style.marginLeft = "37.5%";
+  sidebar.style.display = "block";
+  sidebar.style.width = "25%";
+  open.style.opacity = 0;
+}
+
+function w3_close() {
+  console.log("close called");
+  body.style.marginLeft = "0%";
+  content.style.marginLeft = "25%";
+  sidebar.style.display = "none";
+  open.style.opacity = 1;
+}
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const PORT = 5500;
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
