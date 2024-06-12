5a3bbc7e63208e6e32f4f593d001585a35ebc304
My-Epic-Github <bananazzzzz020@gmail.com>
2024-06-11 21:43:00 -0600

    adding sources and stuff

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.get("/history-of-globalisation", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "history.html"));
 })
 
+app.get("/sources", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "sources.html"));
+})
+
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 });
